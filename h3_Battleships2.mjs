f303830bbe11305de8c4c84f52cc7e45eed10ec4
jente_tav <54480755+jente01t@users.noreply.github.com>
2023-10-25 15:43:41 +0200

    Update h3_Battleships2.mjs

--- h3_Battleships2.mjs
+++ h3_Battleships2.mjs
@@ -15,29 +15,54 @@ let veld = [
     ["[ ]", "[ ]", "[ ]", "[ ]","[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]"],
 ]
 
-//let bootLengte = parseFloat (await userInput.question('Geef de lengte van de boot: '));
-//let bootOrianatie = parseFloat (await userInput.question('Geef de oriantatie van de boot: '));
-//let bootX = parseFloat(await userInput.question('Geef de X coördinaat van de boot: '))
-//let bootY = parseFloat(await userInput.question('Geef de Y coördinaat van de boot: '))
+let bootLengte = parseFloat (await userInput.question('Geef de lengte van de boot: '));
+let bootOrianatie = parseFloat (await userInput.question('Geef de oriantatie van de boot (up, down, right, left): '));
+let bootX = parseFloat(await userInput.question('Geef de X coördinaat van de boot: '))
+let bootY = parseFloat(await userInput.question('Geef de Y coördinaat van de boot: '))
 
 
 let x = parseFloat(await userInput.question('Geef de X coördinaat van het schot: '));
 let y = parseFloat(await userInput.question('Geef de Y coördinaat van het schot: '));
 
 function schiet (x, y) {
-    if (x < 0 || x >= 10 ||y < 0 || y >= 10) {
+    if (x < 0 || x >= 10 || y < 0 || y >= 10) {
         console.log('Je schiet uit het veld.')
+        process.exit();
     } else {
         veld[y-1][x-1] = "[X]";
     }
+    return veld;
 }
 
-//function boot (bootLengte, bootOrianatie) {
-    
-//}
+function boot (bootLengte, bootOrianatie, bootX, bootY) {
+    if (bootOrianatie == "up") {
+        if (bootY - bootLengte < 0) {
+            console.log('Je boot staat uit het veld.');
+            process.exit();
+        }
+    } else if (bootOrianatie == "down") {
+        if (bootY + bootLengte > 10)  {
+            console.log('Je boot staat uit het veld.');
+            process.exit();
+        }
+    } else if (bootOrianatie == "left") {
+        if (bootX - bootLengte < 0) {
+            console.log('Je boot staat uit het veld.');
+            process.exit();
+        }
+    } else if (bootOrianatie == "right") {
+        if (bootX + bootLengte > 10) {
+            console.log('Je boot staat uit het veld.');
+            process.exit();
+        }
+    }
+}
 
-console.log(schiet(x, y));
+schiet(x, y);
 
+for (let i = 0; i < veld.length; i++) {
+    console.log(veld[i].join(" "));
+}
 
 
 process.exit();
