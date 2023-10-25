790a44e26448cd4dbbc2fc64c13c36ea185ca654
jente_tav <54480755+jente01t@users.noreply.github.com>
2023-10-25 15:02:25 +0200

    update main

--- h2_battleships.mjs
+++ h2_battleships.mjs
@@ -2,3 +2,30 @@ import * as readline from 'node:readline/promises';
 import{stdin as input, stdout as output} from 'node:process';
 const userInput = readline.createInterface({input, output});
 
+let veld = [
+    ["[ ]", "[ ]", "[ ]", "[ ]","[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]"],
+    ["[ ]", "[ ]", "[ ]", "[ ]","[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]"],
+    ["[ ]", "[ ]", "[ ]", "[ ]","[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]"],
+    ["[ ]", "[ ]", "[ ]", "[ ]","[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]"],
+    ["[ ]", "[ ]", "[ ]", "[ ]","[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]"],
+    ["[ ]", "[ ]", "[ ]", "[ ]","[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]"],
+    ["[ ]", "[ ]", "[ ]", "[ ]","[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]"],
+    ["[ ]", "[ ]", "[ ]", "[ ]","[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]"],
+    ["[ ]", "[ ]", "[ ]", "[ ]","[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]"],
+    ["[ ]", "[ ]", "[ ]", "[ ]","[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]"],
+]
+
+let x = parseFloat(await userInput.question('Geef de breedte van het schot: '));
+let y = parseFloat(await userInput.question('Geef de hoogte van het schot: '));
+
+function schiet (x, y) {
+    veld[y-1][x-1] = "[X]";
+}
+
+schiet(x, y);
+
+for (let i = 0; i < veld.length; i++) {
+    console.log(veld[i].join(" "));
+}
+
+process.exit();
