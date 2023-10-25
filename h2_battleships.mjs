684ddb910168084642078b6f943535f9e84a1456
jente_tav <54480755+jente01t@users.noreply.github.com>
2023-10-25 14:33:48 +0200

    update main

--- h2_battleships.mjs
+++ h2_battleships.mjs
@@ -2,10 +2,3 @@ import * as readline from 'node:readline/promises';
 import{stdin as input, stdout as output} from 'node:process';
 const userInput = readline.createInterface({input, output});
 
-let getallen = [1, 5, 40 ,20 ,15 ,97, 0, 9, 3, 10];
-
-getallen.sort(function(a, b){return a - b});
-
-console.log(getallen);
-
-process.exit();
