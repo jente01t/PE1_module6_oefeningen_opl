89dc99fc67e1f313572c8e09259543289ea5f9ba
jente_tav <54480755+jente01t@users.noreply.github.com>
2023-10-25 19:58:45 +0200

    Update @2_battleships.mjs

--- @2_battleships.mjs
+++ @2_battleships.mjs
@@ -2,7 +2,7 @@ import * as readline from 'node:readline/promises';
 import { stdin as input, stdout as output } from 'node:process';
 const userInput = readline.createInterface({ input, output });
 
-let veld = [
+let veldSpeler1 = [
     ["[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]"],
     ["[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]"],
     ["[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]"],
@@ -15,26 +15,43 @@ let veld = [
     ["[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]"],
 ]
 
-let bootLengte = parseFloat(await userInput.question('Geef de lengte van de boot: '));
-let bootOrianatie = await userInput.question('Geef de oriantatie van de boot (up, down, right, left): ');
-let bootX = parseFloat(await userInput.question('Geef de X coördinaat van de boot: '));
-let bootY = parseFloat(await userInput.question('Geef de Y coördinaat van de boot: '));
+let veldSpeler2 = [
+    ["[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]"],
+    ["[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]"],
+    ["[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]"],
+    ["[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]"],
+    ["[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]"],
+    ["[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]"],
+    ["[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]"],
+    ["[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]"],
+    ["[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]"],
+    ["[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]"],
+]
+
+let bootLengte = 0;
+let aantalBoten = 0;
 let raak = 0;
 
+while (aantalBoten <= 2) {
+    bootLengte = parseFloat(await userInput.question('Geef de lengte van de boot: '));
+    let bootOrianatie = await userInput.question('Geef de oriantatie van de boot (up, down, right, left): ');
+    let bootX = parseFloat(await userInput.question('Geef de X coördinaat van de boot: '));
+    let bootY = parseFloat(await userInput.question('Geef de Y coördinaat van de boot: '));
+    boot(bootLengte, bootOrianatie, bootX, bootY);
+    console.log('');
+}
 
-boot(bootLengte, bootOrianatie, bootX, bootY);
 
-
-for (let i = 0; i < veld.length; i++) {
-    console.log(veld[i].join(" "));
+for (let i = 0; i < veldSpeler1.length; i++) {
+    console.log(veldSpeler1[i].join(" "));
 }
 
 while (raak < bootLengte) {
     let x = parseFloat(await userInput.question('Geef de breedte van het schot: '));
     let y = parseFloat(await userInput.question('Geef de hoogte van het schot: '));
     schiet(x, y);
-    for (let i = 0; i < veld.length; i++) {
-        console.log(veld[i].join(" "));
+    for (let i = 0; i < veldSpeler1.length; i++) {
+        console.log(veldSpeler1[i].join(" "));
     }
 }
 
@@ -51,38 +68,42 @@ function boot(bootLengte, bootOrianatie, bootX, bootY) {
     if (bootLengte <= 5 && bootLengte >= 2) {
         if (bootOrianatie == "up") {
             if (bootY - bootLengte < 0) {
-                console.log('Je boot staat uit het veld.');
+                console.log('Je boot staat uit het veldSpeler1.');
                 process.exit();
             } else {
                 for (i = 0; i < bootLengte; i++) {
-                    veld[bootY - 1 - i][bootX - 1] = "[B]";
+                    veldSpeler1[bootY - 1 - i][bootX - 1] = "[B]";
+                    aantalBoten++;
                 }
             }
         } else if (bootOrianatie == "down") {
             if (bootY + bootLengte > 10) {
-                console.log('Je boot staat uit het veld.');
+                console.log('Je boot staat uit het veldSpeler1.');
                 process.exit();
             } else {
                 for (i = 0; i < bootLengte; i++) {
-                    veld[bootY - 1 + i][bootX - 1] = "[B]";
+                    veldSpeler1[bootY - 1 + i][bootX - 1] = "[B]";
+                    aantalBoten++;
                 }
             }
         } else if (bootOrianatie == "left") {
             if (bootX - bootLengte < 0) {
-                console.log('Je boot staat uit het veld.');
+                console.log('Je boot staat uit het veldSpeler1.');
                 process.exit();
             } else {
                 for (i = 0; i < bootLengte; i++) {
-                    veld[bootY - 1][bootX - 1 - i] = "[B]";
+                    veldSpeler1[bootY - 1][bootX - 1 - i] = "[B]";
+                    aantalBoten++;
                 }
             }
         } else if (bootOrianatie == "right") {
             if (bootX + bootLengte > 10) {
-                console.log('Je boot staat uit het veld.');
+                console.log('Je boot staat uit het veldSpeler1.');
                 process.exit();
             } else {
                 for (i = 0; i < bootLengte; i++) {
-                    veld[bootY - 1][bootX - 1 + i] = "[B]";
+                    veldSpeler1[bootY - 1][bootX - 1 + i] = "[B]";
+                    aantalBoten++;
                 }
             }
         }
@@ -96,16 +117,17 @@ function boot(bootLengte, bootOrianatie, bootX, bootY) {
 
 function schiet(x, y) {
     if (x > 10 || x < 1 || y > 10 || y < 1) {
-        console.log("Je schot is buiten het veld.");
+        console.log("Je schot is buiten het veldSpeler1.");
     } else {
-        if (veld[y - 1][x - 1] == "[B]") {
+        if (veldSpeler1[y - 1][x - 1] == "[B]") {
             console.log("Raak!");
-            veld[y - 1][x - 1] = "[*]";
+            console.log('');
+            veldSpeler1[y - 1][x - 1] = "[*]";
             raak++;
             return raak;
         } else {
             console.log("Mis!");
-            veld[y - 1][x - 1] = "[^]";
+            veldSpeler1[y - 1][x - 1] = "[^]";
         }
     }
 }
