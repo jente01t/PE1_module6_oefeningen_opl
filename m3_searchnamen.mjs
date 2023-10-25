74aedd17e1c8fed252abcedf90160effaba16b69
jente_tav <54480755+jente01t@users.noreply.github.com>
2023-10-25 14:28:08 +0200

    update main

--- m3_searchnamen.mjs
+++ m3_searchnamen.mjs
@@ -1,14 +1,16 @@
 import * as readline from 'node:readline/promises';
-import{stdin as input, stdout as output} from 'node:process';
-const userInput = readline.createInterface({input, output});
+import { stdin as input, stdout as output } from 'node:process';
+const userInput = readline.createInterface({ input, output });
 
 let naam = await userInput.question('Geef de naam die je zoekt: ');
 let namen = ["Amin", "Sofie", "Samantha", "Karel", "Mehdi", "Joris", "Nuria"]
 
-function zoekNaam (namen, naam) {
+function zoekNaam(namen, naam) {
     for (let i = 0; i < namen.length; i++) {
         if (naam == namen[i]) {
             console.log(i);
+        } else if (i = namen.length) {
+            console.log("-1");
         }
     }
 }
